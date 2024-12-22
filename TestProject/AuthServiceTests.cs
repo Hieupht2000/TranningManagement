c641c9e10001221d5b2bf987f4ddef0c9ee0a142
Hieupht1009 <51768787+Hieuphtpc00147@users.noreply.github.com>
2024-12-22 11:12:45 +0700

    wd

---
+++ TestProject/AuthServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using TranningManagement.Controllers;
+using TranningManagement.Model;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace TestProject
+{
+    public class AuthServiceTests
+    {
+        private readonly UserLoginController _userLoginController;
+
+        [Fact]
+        public void Login_WithCorrectCredentials_ReturnsTrue()
+        {
+            // Arrange
+            //var authService = new AuthService();
+
+            UserloginDTO userLoginDTO = new UserloginDTO
+            {
+                Email = "testuser",
+                password_hash = "password123"
+            };
+
+            var result = _userLoginController.Login(userLoginDTO);
+            
+            // Assert
+            Assert.True(result.IsCompletedSuccessfully);
+        }
+
+        [Fact]
+        public void Login_WithIncorrectCredentials_ReturnsFalse()
+        {
+            // Arrange
+            //var authService = new AuthService();
+            // Arrange
+            var userLoginDTO = new UserloginDTO
+            {
+                Email = "testuser@example.com",
+                password_hash = "wrongpassword"
+            };
+
+            // Act
+            var result = _userLoginController.Login(userLoginDTO);
+
+            // Assert
+            Assert.False(result.IsCanceled);
+        }
+    }
+}
